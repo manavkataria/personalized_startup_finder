00f886eea8c35a37c2dc3417e55ba3243d6c26b7
Aditya Anil Dhoke <adhoke@splunk.com>
2016-02-21 01:43:23 -0800

    modified:   glassdoor_rest.py

--- data_source/glassdoor_rest.py
+++ data_source/glassdoor_rest.py
@@ -1,9 +1,13 @@
 import urllib2, sys
 from BeautifulSoup import BeautifulSoup
 
-url = "http://api.glassdoor.com/api/api.htm?t.p=55690&t.k=em66CFmfXYu&userip=172.23.227.50&useragent=Mozilla&format=json&v=1&action=employers&q="
-hdr = {'User-Agent': 'Mozilla/5.0'}
-req = urllib2.Request(url,headers=hdr)
-response = urllib2.urlopen(req)
-soup = BeautifulSoup(response)
-print soup
+def getGlassdoor(company):
+	url = "http://api.glassdoor.com/api/api.htm?t.p=55690&t.k=em66CFmfXYu&userip=172.23.227.50&useragent=Mozilla&format=json&v=1&action=employers&q="+company
+	hdr = {'User-Agent': 'Mozilla/5.0'}
+	req = urllib2.Request(url,headers=hdr)
+	response = urllib2.urlopen(req)
+	soup = BeautifulSoup(response)
+	print soup
+	return soup
+
+getGlassdoor('apple')
